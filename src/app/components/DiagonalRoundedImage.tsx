798bb41c61142e815af1506827f00139822feac0
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-09-27 19:42:46 -0300

    fix svg cuts to diferent botto mtitle sizes

--- src/app/components/DiagonalRoundedImage.tsx
+++ src/app/components/DiagonalRoundedImage.tsx
@@ -2,9 +2,42 @@ type Props = {
   imgUrl: string;
   alt: string;
   cardId: string;
+  bottomTitleLength:number;
 };
 
-export const DiagonalRoundedImage = ({ imgUrl, alt, cardId }: Props) => {
+export const DiagonalRoundedImage = ({ imgUrl, alt, cardId, bottomTitleLength }: Props) => {
+  console.log(bottomTitleLength)
+  const bottomCutOptionsByTitleLength = {
+    default:`
+        H121.5
+        V385.5
+        C121.5 374.454 130.454 365.5 141.5 365.5
+
+        H325 
+        C336.046 365.5 345 356.546 
+    `,
+    moreThanSix:`
+        H51.5
+        V385.5
+        C51.5 374.454 60.454 365.5 71.5 365.5
+        
+        H325 
+        C336.046 365.5 345 356.546 
+    `,
+    moreThanSeven:`
+        H121.5
+        V365.5
+        C121.5 354.454 130.454 345.5 141.5 345.5
+        
+        H336 
+        C346.046 345.5 355 316.546  
+    `,
+  }
+  const returnCurrentCardCutSize = (bottomTitleLength:number) => {
+    if (bottomTitleLength > 7) return bottomCutOptionsByTitleLength.moreThanSeven
+    if (bottomTitleLength > 6) return bottomCutOptionsByTitleLength.moreThanSix
+    return bottomCutOptionsByTitleLength.default
+  }
   return (
     <svg
       width="299"
@@ -28,7 +61,9 @@ export const DiagonalRoundedImage = ({ imgUrl, alt, cardId }: Props) => {
       </defs>
 
       <path
-        d="M20 0C8.9543 0 0 8.95431 0 20V394.5C0 405.546 8.95431 414.5 20 414.5H121.5V385.5C121.5 374.454 130.454 365.5 141.5 365.5H325C336.046 365.5 345 356.546 345 345.5V85.4073C345 79.7375 342.594 74.3342 338.379 70.5414L265.705 5.13412C262.032 1.82888 257.266 0 252.325 0H20Z"
+        d={`M20 0C8.9543 0 0 8.95431 0 20V394.5C0 405.546 8.95431 414.5 20 414.5
+        ${returnCurrentCardCutSize(bottomTitleLength)}
+        345 345.5 V85.4073C345 79.7375 342.594 74.3342 338.379 70.5414L265.705 5.13412C262.032 1.82888 257.266 0 252.325 0H20Z`}
         fill={`url(#${cardId})`}
       />
     </svg>
--- src/app/components/ServiceCard.tsx
+++ src/app/components/ServiceCard.tsx
@@ -13,9 +13,9 @@ export const ServiceCard = ({upperText, bottomText, title, detailsList, priceTex
       <div className=''>
         <div className={`w-full relative flex flex-col items-center p-2  border-4 border-accent-yellow bg-radial from-white from-60% to-gray-400 rounded-lg text-black mb-10`}>
           <div>
-            <h3 className='title font-black text-left text-3xl uppercase ml-4 -mb-4'>{upperText}</h3>
-            <DiagonalRoundedImage imgUrl={imgUrl} alt={title} cardId={imgUrl}  />
-           <div className="bottomTitleContainer  w-[63%] h-[70px] pb-[2px] pt-[8px] pl-[8px] absolute bottom-0 right-0 overflow-hidden rounded-t-[20px]" >
+            <h3 className='title font-black text-left text-3xl uppercase ml-1 -mb-4'>{upperText}</h3>
+            <DiagonalRoundedImage imgUrl={imgUrl} alt={title} cardId={imgUrl} bottomTitleLength={bottomText.length}  />
+           <div className="bottomTitleContainer  w-[63%] h-[70px] pb-[2px] pt-[8px] pl-[8px] absolute bottom-0 right-0 overflow-hidden r" >
               <h3 className='title font-black text-3xl uppercase '>{bottomText}</h3>
             </div>
           </div>
