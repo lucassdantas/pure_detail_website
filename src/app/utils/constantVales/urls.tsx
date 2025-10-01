3f062a4d7147c8f3f64691f842299ecd567d0933
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-10-01 18:57:57 -0300

    change website url

--- src/app/utils/constantVales/urls.tsx
+++ src/app/utils/constantVales/urls.tsx
@@ -1,4 +1,4 @@
 export const imgsDirectory:string = '/imgs'
 export const servicesImgsDirectory:string = imgsDirectory+'/services'
 export const transformationsImgsDirectory:string = imgsDirectory+'/transformations'
-export const siteUrl = 'https://puredetail.devdantas.com.br/'
+export const siteUrl = 'https://puredetail.co.nz'
