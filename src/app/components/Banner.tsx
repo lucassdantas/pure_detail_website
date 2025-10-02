375fd097d3b506ea1d7fcd6973f5b008bcb1f9ab
Lucas Dantas <lucasdantasprogramador@gmail.com>
2025-10-02 20:37:19 -0300

    add og banner img, fix letter spacing on bannner and highlighted titles

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -24,7 +24,7 @@ export const Banner = ({bgImgClass, title, hasButton, button}:BannerProps) => {
   return (
     <Section className={`relative flex justify-center py-24 items-end text-center min-h-[70vh] bannerSection text-white ${bgImgClass}`}>
       <div className={`absolute h-full w-full top-0 left-0 opacity-60 bg-black `}></div>
-      <Title tag='h1' className={`uppercase font-black relative lg:text-7xl text-4xl mb-24 `}>{title}</Title>
+      <Title tag='h1' className={`uppercase font-black relative lg:text-7xl text-4xl mb-24 -tracking-wider `}>{title}</Title>
       {hasButton &&<Button url={button?.url} text={button?.text} textSize='lg:text-4xl text-2xl' className='hover:border-accent-yellow px-6'/>}
     </Section>
   )
